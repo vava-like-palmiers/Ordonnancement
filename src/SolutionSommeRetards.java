9259578689f5a24c4ad3dd2de5ee4d09efe14b80
vava-like-palmiers <vive-les-palmiersx2@live.fr>
2018-04-17 16:02:19 +0200

    double shit

--- src/SolutionSommeRetards.java
+++ src/SolutionSommeRetards.java
@@ -1,11 +1,17 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 
 public class SolutionSommeRetards implements SolutionPartielle {
 
+	private List<Processus> procs;
+	
 	@Override
 	public int valeur() {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
@@ -13,6 +19,20 @@ public class SolutionSommeRetards implements SolutionPartielle {
 	public List<SolutionPartielle> voisins() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public static SolutionPartielle borneSup(){
+		SolutionSommeRetards sol = new SolutionSommeRetards();
+		sol.procs.addAll(enonce);
+		Collections.sort(sol.procs, new Comparator<Processus>() {
+
+			@Override
+			public int compare(Processus arg0, Processus arg1) {
+				return arg0.di/arg0.wi < arg1.di/arg1.wi ? +1 : -1;
+			}
+		});
+		
+		return sol;
 	}
 
 }
